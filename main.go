bc7eed9002ee1c7d3c25a332b9a397159cfcc7ab
efren <efren.hernandez@alumnos.udg.mx>
2020-09-29 12:00:24 -0500

    v1

--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+
+	var limite float64
+	var producto float64 = 1
+	var suma float64 = 1
+	var i float64 = 1
+
+	fmt.Scanln(&limite)
+	for i <= limite {
+		producto = producto + i
+		suma = suma + (1 / producto)
+		i = i + 1
+	}
+	fmt.Print(suma)
+
 }
